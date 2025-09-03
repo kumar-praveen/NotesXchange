56d776e51b7a21998c1948451ed55f6db3ff7193
Praveen Raj <praveenraj7667@gmail.com>
2025-09-03 16:45:44 +0530

    fixed cookie related issue

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -202,6 +202,7 @@ export const login = async (req, res) => {
       .cookie("token", token, {
         maxAge: 24 * 60 * 60 * 1000,
         httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
         sameSite: "none",
       })
       .json({
